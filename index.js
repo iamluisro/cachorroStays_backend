359acb61590df6e7eaa3b7b0444bee8ab22039c5
lfrodriguez <lfrodriguez06@gmail.com>
2019-11-12 00:34:11 -0600

    error handler created

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const app = express();
 const { config } = require('./config/index');
 const hotelsApi = require('./routes/hotels.js');
 
+const {
+  logErrors,
+  errorHandler
+} = require('./utils/middleware/errorHandlers.js');
+
 //body_parser middleware
 app.use(express.json());
+
 hotelsApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function() {
   console.log(`listening http://localhost:${config.port}`);
---
+++ utils/middleware/errorHandlers.js
@@ -0,0 +1,25 @@
+const { config } = require('../../config');
+
+function withErrorStack(error, stack) {
+  if (config.dev) {
+    return { error, stack };
+  }
+
+  return error;
+}
+
+function logErrors(err, req, res, next) {
+  console.log(err);
+  next(err);
+}
+
+// eslint-disable-next-line no-unused-vars
+function errorHandler(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json(withErrorStack(err.message, err.stack));
+}
+
+module.exports = {
+  logErrors,
+  errorHandler
+};
