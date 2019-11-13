4db740e0b5c42815545e9c6f2b18707ad333407d
lfrodriguez <lfrodriguez06@gmail.com>
2019-11-12 23:55:33 -0600

    validationHandler was created installing @hapi/joi , the hotelSchema was created and implemented into the routes

--- index.js
+++ index.js
@@ -6,15 +6,24 @@ const hotelsApi = require('./routes/hotels.js');
 
 const {
   logErrors,
-  errorHandler
+  errorHandler,
+  wrapError
 } = require('./utils/middleware/errorHandlers.js');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler.js');
 
 //body_parser middleware
 app.use(express.json());
 
+//Routes
 hotelsApi(app);
 
+//Catch 404 error
+app.use(notFoundHandler);
+
+//Error Middleware
 app.use(logErrors);
+app.use(wrapError);
 app.use(errorHandler);
 
 app.listen(config.port, function() {
