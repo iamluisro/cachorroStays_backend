eeea6d2885d3db9b7bcdba2d2704881ca2ba690d
lfrodriguez <lfrodriguez06@gmail.com>
2019-11-20 16:42:02 -0600

    added google authentication to the proyect

---
+++ ssr-server/index.js
@@ -0,0 +1,134 @@
+const express = require("express");
+const passport = require("passport");
+const boom = require("@hapi/boom");
+const cookieParser = require("cookie-parser");
+const axios = require("axios");
+
+const { config } = require("./config");
+
+const app = express();
+
+// body parser
+app.use(express.json());
+app.use(cookieParser());
+
+//Basic Strategy
+require("./utils/auth/strategies/basic");
+
+//OauthStrategy
+require("./utils/auth/strategies/oauth");
+
+app.post("/auth/sign-in", async function(req, res, next) {
+  passport.authenticate("basic", function(error, data) {
+    try {
+      if (error || !data) {
+        next(boom.unauthorized());
+      }
+
+      req.login(data, { session: false }, async function(error) {
+        if (error) {
+          next(error);
+        }
+        const { token, ...user } = data;
+
+        res.cookie("token", token, {
+          httpOnly: !config.dev,
+          secure: !config.dev
+        });
+
+        res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
+});
+
+app.post("/auth/sign-up", async function(req, res, next) {
+  const { body: user } = req;
+
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: "post",
+      data: user
+    });
+
+    res.status(201).json({ message: "User created" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/hotels", async function(req, res, next) {});
+
+app.post("/user-hotels", async function(req, res, next) {
+  try {
+    const { body: userHotel } = req;
+    const { token } = req.cookies;
+
+    const { data, satus } = await axios({
+      url: `${cong.apiUrl}/api/user-hotels`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: "post",
+      data: userHotel
+    });
+    if (status !== 201) {
+      return next(boom.badImplementation());
+    }
+
+    res.status(201).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete("/user-hotels/:userHotelId", async function(req, res, next) {
+  try {
+    const { userHotelId } = req;
+    const { token } = req.cookies;
+
+    const { data, status } = await axios({
+      url: `${cong.apiUrl}/api/user-hotels/${userHotelId}`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: "delete"
+    });
+    if (status !== 200) {
+      return next(boom.badImplementation());
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get(
+  "/auth/google-oauth",
+  passport.authenticate("google-oauth", {
+    scope: ["email", "profile", "openid"]
+  })
+);
+
+app.get(
+  "/auth/google-oauth/callback",
+  passport.authenticate("google-oauth", { session: false }),
+  function(req, res, next) {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie("token", token, {
+      httpOnly: !config.dev,
+      secure: !config.dev
+    });
+
+    res.status(200).json(user);
+  }
+);
+
+app.listen(config.port, function() {
+  console.log(`Listening http://localhost:${config.port}`);
+});
